329776391b4569c7e75bd6097be50152849ccba7
Zhiming Wang <pypi@snh48live.org>
2018-10-20 15:49:22 -0400

    config: handle NTFS invalid characters
    Replace displayable characters with homographs (just FULLWIDTH
    versions), and strip control characters.
    
    THIS IS A BACKWARD-INCOMPATIBLE CHANGE.

--- src/kvm48/config.py
+++ src/kvm48/config.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 from typing import List
 
@@ -98,7 +99,27 @@ class Config(object):
             "title": vod.title.strip(),
             "ext": os.path.splitext(vod.vod_url)[1][1:],
         }
-        return unsanitized.translate(str.maketrans("/\t\n\r\f\v", "_     "))
+        # Strip control characters (0x00-0x1F, 0x7F), and use homoglyphs
+        # (Halfwidth and Fullwidth Forms block, U+FF00 - U+FFEF) for
+        # characters illegal in exFAT/NTFS:
+        #
+        # " => U+FF02 FULLWIDTH QUOTATION MARK (＂)
+        # * => U+FF0A FULLWIDTH ASTERISK (＊)
+        # / => U+FF0F FULLWIDTH SOLIDUS (／)
+        # : => U+FF1A FULLWIDTH COLON (：)
+        # < => U+FF1C FULLWIDTH LESS-THAN SIGN (＜)
+        # > => U+FF1E FULLWIDTH GREATER-THAN SIGN (＞)
+        # ? => U+FF1F FULLWIDTH QUESTION MARK (？)
+        # \ => U+FF3C FULLWIDTH REVERSE SOLIDUS (＼)
+        # | => U+FF5C FULLWIDTH VERTICAL LINE (｜)
+        #
+        # Also replace whitespace characters with the space
+        return re.sub(r"[\x00-\x1f\x7f]+", "", unsanitized).translate(
+            str.maketrans(
+                '"*/:<>?\\|\t\n\r\f\v',
+                "\uFF02\uFF0A\uFF0F\uFF1A\uFF1C\uFF1E\uFF1F\uFF3C\uFF5C     ",
+            )
+        )
 
     def load(self, config_file: str = None) -> None:
         if not config_file:
