8ff8e478f87aa3332f932e228e458f5f9fe4d286
Zhiming Wang <pypi@snh48live.org>
2018-10-28 02:19:34 -0400

    Drop (official) Python 3.5 support
    Now that we're natively integrated with caterpillar, and caterpillar requires
    Python 3.6 (although we don't really need Python-level integration)

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setuptools.setup(
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
