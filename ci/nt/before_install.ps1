b91ddb7f25c881f0098d5825f2b08e75afd52650
Zhiming Wang <pypi@snh48live.org>
2018-11-22 15:51:05 -0500

    ci/nt: consolidate another series of attempts

--- ci/nt/before_install.ps1
+++ ci/nt/before_install.ps1
@@ -1,6 +1,21 @@
+﻿$ErrorActionPreference = "Stop"
+Get-WindowsFeature
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-choco install -y aria2 ffmpeg python
+ls C:\Windows\System32
+choco install -y aria2 python
+# Download and install a static build of FFmpeg due to DLL loading difficulties:
+# https://github.com/SNH48Live/KVM48/issues/4
+# At the moment the downloaded build is a nightly; I've contacted the packager
+# to see if he could provide a static URL for the latest stable.
+Invoke-WebRequest "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip" -OutFile ffmpeg.zip
+Expand-Archive ffmpeg.zip .
+Copy-Item "ffmpeg-latest-win32-static\bin\ffmpeg.exe" "$env:ChocolateyInstall\bin"
 refreshenv
 pip install wheel
+Get-Command ffmpeg
 ffmpeg -version
+if ($LastExitCode -ne 0) {
+    throw "ffmpeg failed with $LastExitCode"
+}
+Get-Command python
 python --version
--- ci/nt/install.ps1
+++ ci/nt/install.ps1
@@ -1,4 +1,4 @@
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+﻿Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 pip install --pre caterpillar-hls
 pip install .
